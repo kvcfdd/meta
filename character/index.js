6dadf28bdae7331fca9bdfd7e11da42269a49710
ω <1950553426@qq.com>
2026-06-30 01:10:03 +0800

    上传资源

--- character/index.js
+++
@@ -1,8 +0,0 @@
-import { Data, Meta } from "#miao"
-import { alias } from "./alias.js"
-import lodash from "lodash"
-
-let data = Data.readJSON("resources/meta-mc/character/data.json", "miao")
-let meta = Meta.create("mc", "char")
-meta.addData(data)
-meta.addAlias(alias)
